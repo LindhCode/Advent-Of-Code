8d56902e110cf0e4e0a2d231226e020df9c01e80
LindhCode <axellindh2004@gmail.com>
2025-12-02 19:52:32 +0100

    Kommit närmre, men inte hela vägen fram på part2

--- Day-1/part2.py
+++ Day-1/part2.py
@@ -3,33 +3,32 @@ def code_crack(file):
         data = f.readlines()
     counter = 0
     number = 50
-    passed_border = False
+    previous_number = None
     for i in data:
-
+        print(f"Previous number: {previous_number}")
+        print(i)
         number += (int(i[1:]) if i.startswith("R") else -int(i[1:]))
         if  number > 99:
-            #print(number)
-            passed_border = True
-            counter += (number // 100)
+            if previous_number != 0 or number > 100:
+                counter += (number // 100)
+            print("counter 1+: " + str(counter))
+            number = number % 100
+            if number == 0:
+                counter -= 1
             print(number)
-            number = number % 100
-            print(f"Over 99: {counter}")
         elif number < 0:
-            #Problem: Om det passerar 0, räknas inte +1
-            # om number är ex -12
-            passed_border = True
-            #counter += 1
-            counter += (abs(number) // 100)
-
+            if previous_number != 0 or number < -100:
+                counter += (abs(number) // 100) + 1
+            print("counter 1-: " + str(counter))
             number = number % 100
-            print(f"Under 0: {counter}")
+            if number == 0:
+                counter -= 1
+            print(number)
         if number == 0:
             counter += 1
-
-        passed_border = False
+        print("counter 2: " + str(counter))
+        print("---------------------------")
+        previous_number = number
     return counter
 
-def correct_count_number(number, counter):
-    pass
-
-print(code_crack("test.txt"))
+print(code_crack("data.txt"))
